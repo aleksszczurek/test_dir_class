d54f03e858e598a5e58aa9100d2259f78b1d8bc7
aleksszczurek <aleksander.szczurek@bioch.ox.ac.uk>
2018-10-18 15:10:09 +0100

    thursdays changes

--- scripts/scriptBMI.R
+++ scripts/scriptBMI.R
@@ -2,7 +2,7 @@
 
 ####Input your weight in kg
 
-weight <- 50
+weight <- 78
 
 ###Input your height in m
 
@@ -10,17 +10,22 @@ height <- 1.7
 
 ####Calculate Body Mass Index (BMI)
 
-BMI <- weight / (height^2)
-
-###Get your results
-
-if (BMI < 16) {
-  print('Have a couple more chocolate bars')
-} else {
-  if (BMI >= 16 & BMI < 25) {
-    print('Keep on snacking')
+BMI <- function(weight, height) {
+  BMI <- weight / (height^2)
+  
+  ###Get your results
+  
+  if (BMI < 16) {
+    print('Have a couple more chocolate bars')
   } else {
-    if (BMI >= 25) {
-      print('Maybe skip that one chocolate bar before sleep')
+    if (BMI >= 16 & BMI < 25) {
+      print('Keep on snacking')
+    } else {
+      if (BMI >= 25) {
+        print('Maybe skip that one chocolate bar before sleep')
+      }
     }
   }
+  }
+
+BMI(67, 1.76)
